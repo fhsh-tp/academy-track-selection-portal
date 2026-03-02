6abe7475371977e7134594a920099147660ac924
Na2CO3 <chenberlin179@gmail.com>
2026-03-02 16:41:21 +0800

    fix: replace passlib with bcrypt to fix deployment crash

--- backend/security.py
+++ backend/security.py
@@ -6,7 +6,7 @@ from fastapi import Depends, HTTPException, status
 from fastapi.security import OAuth2PasswordBearer
 
 # 設定
-SECRET_KEY = os.environ.get("SECRET_KEY")
+SECRET_KEY = os.environ["SECRET_KEY"]
 ALGORITHM = "HS256"
 oauth2_scheme = OAuth2PasswordBearer(tokenUrl="login")
 
